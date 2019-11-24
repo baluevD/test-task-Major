e602fdd100d8e29835e6c0fe5bdaeca499896321
baluevD <newarmlet@gmail.com>
2019-11-25 01:31:53 +0300

    change hamburger menu blackout effect

--- js/offcanvas.js
+++ js/offcanvas.js
@@ -4,10 +4,13 @@ $(function () {
     $('[data-toggle="offcanvas"]').on('click', function () {
         $('.offcanvas-collapse').addClass('open');
         $('body').addClass('offcanvas-open');
+        $('<div class="overlay">&nbsp;</div>').prependTo($('body'));
+        $(".overlay").show("300");
     })
     $('[data-toggle="offcanvas-close"]').on('click', function () {
         $('.offcanvas-collapse').removeClass('open');
         $('body').removeClass('offcanvas-open');
-
+        $(".overlay").hide(300);
+        $('.overlay').remove();
     })
 })
